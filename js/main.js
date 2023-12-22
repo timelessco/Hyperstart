852d34d2a999cf45e9a118ee4e31b2aa2b16312d
Mohanaprabhu.R <mohan@timeless.co>
2023-12-22 10:44:39 +0530

    First commit

--- js/main.js
+++ js/main.js
@@ -1,158 +1,112 @@
-$(document).ready(function() {
-    //Page scroll add class name
-    $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
-        if (scroll >= 100) {
-            $(".site-header").addClass("header-overlay");
-        } else {
-            $(".site-header").removeClass("header-overlay");
-        }
-    });
-
-
-
-    addAndRemoveClassOneByOne();
-
-
-
-
-});
-
- // Function to add and remove a class to each div one by one
- const addAndRemoveClassOneByOne = () => {
-  let currentIndex = 0;
-  const elements = $('.clm-list-content');
-  const addClass = () => {
-    elements.removeClass('clm-list-selected');
-    $(elements[currentIndex]).addClass('clm-list-selected'); 
-    currentIndex = (currentIndex + 1) % elements.length; 
-
-    setTimeout(addClass, 3000);
-  };
-  addClass();
-};
-addAndRemoveClassOneByOne();
-
-
-
-
-
-
-
-
-
-//Thumnail carousal
-function thumbCarousal() {
-    const mainCarouselWrap = document.getElementById("main-carousel");
-    const mainCarouselView = mainCarouselWrap.querySelector(".embla__viewport");
-    const slides = mainCarouselView.querySelectorAll(".embla__slide");
-    let delay = 4000;
-    const mainCarousel = EmblaCarousel(
-      mainCarouselView,
-      {
-        selectedClass: "",
-        loop: true,
-        skipSnaps: false,
-        startIndex: 0,
-        align: 'start',  
-      },
-      [
-        EmblaCarouselAutoplay(
-          {
-            playOnInit: true,
-            delay: delay,
-          },
-          (emblaRoot) => emblaRoot.parentElement
-        ),
-      ]
-    );
-  
-    const thumbCarouselWrap = document.getElementById("thumb-carousel");
-    const thumbCarouselView = thumbCarouselWrap.querySelector(".embla__viewport");
-    const thumbCarousel = EmblaCarousel(
-      thumbCarouselView,
-      {
-        selectedClass: "",
-        containScroll: "keepSnaps",
-        dragFree: false,
-        startIndex: 0,
-        slidesToScroll: 1,
-        slidesInView: window.innerWidth < 768 ? 1 : 3,
-      },
-      [
-        EmblaCarouselAutoplay(
-          {
-            playOnInit: true,
-            delay: delay,
-          },
-          (emblaRoot) => emblaRoot.parentElement
-        ),
-      ]
-    );
-  
-    const onThumbClick = (mainCarousel, thumbCarousel, index) => () => {
-      mainCarousel.scrollTo(index);
-    };
-  
-    const followMainCarousel = (mainCarousel, thumbCarousel) => () => {
-      const currentSlideIndex = mainCarousel.selectedScrollSnap();
-      thumbCarousel.scrollTo(mainCarousel.selectedScrollSnap());
-      selectThumbBtn(mainCarousel, thumbCarousel);
-      slides.forEach((slide) => {
-        slide.classList.remove("is-current-slide");
-      });
-      slides[currentSlideIndex].classList.add("is-current-slide");
-    };
-    const selectThumbBtn = (mainCarousel, thumbCarousel) => {
-        const selected = mainCarousel.selectedScrollSnap();
-  
-        for (let i = 0; i <= selected; i++) {
-          thumbCarousel.slideNodes()[i].classList.add('is-selected');
-        }
-  
-        for (let i = selected + 1; i < thumbCarousel.slideNodes().length; i++) {
-          thumbCarousel.slideNodes()[i].classList.remove('is-selected');
-        }
-      };
-  
-    thumbCarousel.slideNodes().forEach((thumbNode, index) => {
-      const onClick = onThumbClick(mainCarousel, thumbCarousel, index);
-      thumbNode.addEventListener("click", onClick, false);
-    });
-  
-    const syncThumbCarousel = followMainCarousel(mainCarousel, thumbCarousel);
-    mainCarousel.on("select", syncThumbCarousel);
-    thumbCarousel.on("init", syncThumbCarousel);
-  }
-  
-
-  
-  window.addEventListener("load", () => {
-    thumbCarousal();
-  });
-  
-
-
-
-var Cards = document.querySelectorAll("#cardList > div");
-document.addEventListener("DOMContentLoaded", function () {
-  currentHighlight = 0;
-  const N = 3; // interval in seconds
-  function changeActiveState() {
-    currentHighlight = (currentHighlight + 1) % Cards.length;
-    for (var i = 0; i < Cards.length; i++) {
-      Cards[i].classList.remove("active");
+$(document).ready(function () {
+  // Page scroll add class name
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop()
+    if (scroll >= 100) {
+      $('.site-header').addClass('header-overlay')
+    } else {
+      $('.site-header').removeClass('header-overlay')
     }
-    // console.log("this ", currentHighlight)
-    Cards[currentHighlight].classList.add("active");
+  })
+})
+
+// Thumnail carousal
+
+function thumbCarousal () {
+  const mainCarouselWrap = document.getElementById('main-carousel')
+  const mainCarouselView = mainCarouselWrap.querySelector('.embla__viewport')
+  const prevBtn = document.querySelector('.embla__button--prev')
+  const nextBtn = document.querySelector('.embla__button--next')
+  let delay = 4000
+
+  const mainCarousel = EmblaCarousel(
+    mainCarouselView,
+    {
+      selectedClass: '',
+      loop: true,
+      skipSnaps: false,
+      startIndex: 0,
+      align: 'start'
+    },
+    [
+      EmblaCarouselAutoplay(
+        {
+          playOnInit: true,
+          delay: delay
+        },
+        (emblaRoot) => emblaRoot.parentElement
+      )
+    ]
+  )
+
+  const thumbCarouselWrap = document.getElementById('thumb-carousel')
+  const thumbCarouselView = thumbCarouselWrap.querySelector('.embla__viewport')
+  const thumbCarousel = EmblaCarousel(
+    thumbCarouselView,
+    {
+      selectedClass: '',
+      containScroll: 'keepSnaps',
+      dragFree: false,
+      startIndex: 0,
+      slidesToScroll: 1,
+      slidesInView: window.innerWidth < 768 ? 1 : 3
+    },
+    [
+      EmblaCarouselAutoplay(
+        {
+          playOnInit: true,
+          delay: delay
+        },
+        (emblaRoot) => emblaRoot.parentElement
+      )
+    ]
+  )
+
+  const onThumbClick = (mainCarousel, thumbCarousel, index) => () => {
+    mainCarousel.scrollTo(index)
   }
-  setInterval(changeActiveState, N * 1000);
-});
 
-function hoverActiveState () {
-    Cards[i].classList.add('dropdown-show');
+  const followMainCarousel = (mainCarousel, thumbCarousel) => () => {
+    const currentSlideIndex = mainCarousel.selectedScrollSnap()
+    thumbCarousel.scrollTo(currentSlideIndex)
+    selectThumbBtn(mainCarousel, thumbCarousel)
+  }
+
+  const selectThumbBtn = (mainCarousel, thumbCarousel) => {
+    const selected = mainCarousel.selectedScrollSnap()
+
+    for (let i = 0; i <= selected; i++) {
+      thumbCarousel.slideNodes()[i].classList.add('is-selected')
+    }
+
+    for (let i = selected + 1; i < thumbCarousel.slideNodes().length; i++) {
+      thumbCarousel.slideNodes()[i].classList.remove('is-selected')
+    }
+  }
+
+  thumbCarousel.slideNodes().forEach((thumbNode, index) => {
+    const onClick = onThumbClick(mainCarousel, thumbCarousel, index)
+    thumbNode.addEventListener('click', onClick, false)
+  })
+
+  const syncThumbCarousel = followMainCarousel(mainCarousel, thumbCarousel)
+  mainCarousel.on('select', syncThumbCarousel)
+  thumbCarousel.on('init', syncThumbCarousel)
+
+  const prev = () => {
+    mainCarousel.scrollPrev()
+    syncThumbCarousel()
+  }
+
+  const next = () => {
+    mainCarousel.scrollNext()
+    syncThumbCarousel()
+  }
+
+  prevBtn.addEventListener('click', prev)
+  nextBtn.addEventListener('click', next)
 }
 
-for (var i = 0; i < Cards.length; i++) {
-    Cards.addEventListener('mouseenter', hoverActiveState)
-}
+window.addEventListener('load', () => {
+  thumbCarousal()
+})
